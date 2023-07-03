67c4688f303310a7b34bd8bbb9e032ee7caafe65
Leonardo <https://github.com/Leonardo-tech-stack/sound-garden-test>
2023-07-03 14:50:12 -0300

    .env

--- src/services/mainAPI/config/index.ts
+++ src/services/mainAPI/config/index.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 const mainApiJson = axios.create({
-  baseURL: 'https://api-ecommerce-livraria.onrender.com',
+  baseURL: process.env.REACT_APP_BASE_API_URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
 
 const mainApiMultipart = axios.create({
-  baseURL: 'https://api-ecommerce-livraria.onrender.com',
+  baseURL: process.env.REACT_APP_BASE_API_URL,
   withCredentials: true,
   headers: { "Content-Type": "multipart/form-data" },
 });
 
 const noHeader = axios.create({
-  baseURL: 'https://api-ecommerce-livraria.onrender.com',
+  baseURL: process.env.REACT_APP_BASE_API_URL,
   withCredentials: true,
 });
 
